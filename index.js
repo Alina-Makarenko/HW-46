e856877bbefc7081e8fe026b291737339fc2b67b
Alina-Makarenko <alinamakarenko1807@gmail.com>
2026-01-06 00:56:13 +0200

    gg

---
+++ index.js
@@ -0,0 +1,19 @@
+// 1---
+const buttonEl = document.querySelector(".btn");
+const inputEl = document.querySelector('[type="text"]');
+console.log(buttonEl);
+console.log(inputEl);
+buttonEl.value = inputEl.value;
+// 2
+const imgEl = document.querySelector(".photo");
+imgEl.src =
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjOx-R278aLA2aEWKByAsOo6JotkOvm1fyCA&s";
+// 3
+const linkEl = document.querySelector(".link");
+linkEl.href = "https://znohub.online/courses";
+console.log(linkEl);
+
+const img2El = document.querySelector(".photos");
+img2El.alt = "line5678";
+console.log(img2El);
+// 4
